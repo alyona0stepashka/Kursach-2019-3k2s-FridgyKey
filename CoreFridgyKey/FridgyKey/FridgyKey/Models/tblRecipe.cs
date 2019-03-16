1228d759fbd7ce20879e4a8f2a30cc87266fbfb6
alyona0stepashka <paspaspas01@yandex.ru>
2019-03-16 08:09:28 +0300

    core

---
+++ CoreFridgyKey/FridgyKey/FridgyKey/Models/tblRecipe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FridgyKey.Models
+{
+    [Table("tblRecipe")]
+    public partial class tblRecipe
+    {
+        public int Id { get; set; }
+        public int FrostId { get; set; }
+        public string Name{ get; set; }
+        public string Text { get; set; }
+        public float Kkal { get; set; }
+        public string Notation { get; set; }
+        public float Price { get; set; }
+        public virtual ICollection<tblRecipe> Recipes { get; set; }
+        public tblRecipe()
+        {
+            Recipes = new HashSet<tblRecipe>();
+        }
+    }
+}
