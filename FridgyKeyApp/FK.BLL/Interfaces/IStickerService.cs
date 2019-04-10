b33fe723f8982d781965102feadcd2e6d81a5a83
alyona0stepashka <paspaspas01@yandex.ru>
2019-04-10 12:33:01 +0300

    Update IStickerService.cs

--- FridgyKeyApp/FK.BLL/Interfaces/IStickerService.cs
+++ FridgyKeyApp/FK.BLL/Interfaces/IStickerService.cs
@@ -12,9 +12,7 @@ namespace FK.BLL.Interfaces
         void DeleteByFridgeId(int? fridge_id);  //удалить все записи на этом холодильнике
         IEnumerable<Sticker> GetAll();  //получить все записи всех пользователей на всех холодильниках
         IEnumerable<Sticker> GetAllByUserId(int? user_id);  //получить все записи этого юзера
-        IEnumerable<Sticker> GetAllByFridgeId(int? fridge_id);  //получить все записи на этом холодильнике
-        IEnumerable<Sticker> SearchByWord(string word);  //найти все записи, в которых есть это слово
-        IEnumerable<Sticker> SearchByWord(string word, int? user_id);  //найти записи этого юзера, в которых есть это слово
+        IEnumerable<Sticker> GetAllByFridgeId(int? fridge_id);  //получить все записи на этом холодильнике 
         IEnumerable<Sticker> SearchByWord(string word, int? user_id, int? fridge_id);  //найти записи этого юзера на этом холодильнике, в которых есть это слово
         IEnumerable<Sticker> SearchByDate(DateTime date);  //найти все записи с этой датой
 
