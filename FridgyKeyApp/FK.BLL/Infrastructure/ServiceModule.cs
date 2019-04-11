6728f45b0e620c5ddc13c44506b6187a4e45e324
alyona0stepashka <paspaspas01@yandex.ru>
2019-04-11 13:35:54 +0300

    fix services

---
+++ FridgyKeyApp/FK.BLL/Infrastructure/ServiceModule.cs
@@ -0,0 +1,22 @@
+﻿using FK.DAL.Interfaces;
+using FK.DAL.Repositories;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FK.BLL.Infrastructure
+{
+    public class ServiceModule : NinjectModule
+    {
+        private string connectionString;
+        public ServiceModule(string connection)
+        {
+            connectionString = connection;
+        }
+        public override void Load()
+        {
+            Bind<IUnitOfWork>().To<EFUnitOfWork>().WithConstructorArgument(connectionString);
+        }
+    }
+}
