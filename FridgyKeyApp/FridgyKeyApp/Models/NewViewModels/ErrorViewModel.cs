46958dbefca0310ad87c4df0ea9bfe313185e36f
alyona0stepashka <alyona0stepashka@users.noreply.github.com>
2019-05-22 19:18:48 +0300

    clear master

--- FridgyKeyApp/FridgyKeyApp/Models/NewViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FridgeKeyApp.Models.New
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
